4c79090eda2193d1645d2d1fef441740996dbcf9
Lally <125519164+LallyCupCake@users.noreply.github.com>
2023-11-22 11:19:24 -0300

    Add files via upload

---
+++ menu/script.js
@@ -0,0 +1,8 @@
+function abre() {
+    let menu = document.querySelector('.menu')
+    menu.style.width = '25%'
+}
+function fecha() {
+    let menu = document.querySelector('.menu')
+    menu.style.width = '0'
+}
